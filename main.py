bcb13040adb053225af15b1945c03af1f9a284e6
chongyangwang-song <3227303639@qq.com>
2021-03-12 21:22:08 +0800

    result:13,time:150

--- main.py
+++ main.py
@@ -1,43 +1,78 @@
 import random
 import collections
-
-
+import time
 import sys
+start = time.time()
+SERVER = True
+TIME = False
 # while True:
 #     line = sy
 host_dict = {}
 vm_dict = {}
-PATH = '/data1/HUAWEI/training-1.txt'
-with open(PATH) as f:
-    lines = f.readlines()
+if not SERVER:
+    PATH = '/data1/HUAWEI/training-1.txt'
+    with open(PATH) as f:
+        lines = f.readlines()
 
-lines = [line.strip() for line in lines]
+    lines = [line.strip() for line in lines]
 
-host_num = int(lines[0])
-for i in range(1, host_num+1):
-    line = lines[i]
-    temp = line.split(',')
-    host_name = temp[0][1:]
-    val = [int(temp[1]),int(temp[2]),int(temp[3]),int(temp[4][:-1])]
-    host_dict[host_name] = val
-vm_num = int(lines[host_num+1])
-for i in range(host_num+2,host_num+2+vm_num):
-    line = lines[i]
-    temp = line.split(',')
-    vm_name = temp[0][1:]
-    val = [int(temp[1]), int(temp[2]),int(temp[3][:-1])]
-    vm_dict[vm_name] = val
-day_num = int(lines[host_num+2+vm_num])
-data_index = host_num+2+vm_num+1
-data = []
-for i in range(day_num):
-    data_i = []
-    data_num = int(lines[data_index])
-    for item in lines[data_index+1:data_index+1+data_num]:
-        data_i.append(item)
-    data.append(data_i)
-    data_index = data_index+data_num+1
+    host_num = int(lines[0])
+    for i in range(1, host_num+1):
+        line = lines[i]
+        temp = line.split(',')
+        host_name = temp[0][1:]
+        val = [int(temp[1]),int(temp[2]),int(temp[3]),int(temp[4][:-1])]
+        host_dict[host_name] = val
+    vm_num = int(lines[host_num+1])
+    for i in range(host_num+2,host_num+2+vm_num):
+        line = lines[i]
+        temp = line.split(',')
+        vm_name = temp[0][1:]
+        val = [int(temp[1]), int(temp[2]),int(temp[3][:-1])]
+        vm_dict[vm_name] = val
+    day_num = int(lines[host_num+2+vm_num])
+    data_index = host_num+2+vm_num+1
+    data = []
+    for i in range(day_num):
+        data_i = []
+        data_num = int(lines[data_index])
+        for item in lines[data_index+1:data_index+1+data_num]:
+            data_i.append(item)
+        data.append(data_i)
+        data_index = data_index+data_num+1
+else:
+    host_num = sys.stdin.readline().strip()
+    host_num = int(host_num)
+    # host_num = int(lines[0])
+    for i in range(host_num):
+        line = sys.stdin.readline().strip()
+        temp = line.split(',')
+        host_name = temp[0][1:]
+        val = [int(temp[1]), int(temp[2]), int(temp[3]), int(temp[4][:-1])]
+        host_dict[host_name] = val
 
+    vm_num = sys.stdin.readline().strip()
+    vm_num = int(vm_num)
+    for i in range(vm_num):
+        line = sys.stdin.readline().strip()
+        temp = line.split(',')
+        vm_name = temp[0][1:]
+        val = [int(temp[1]), int(temp[2]), int(temp[3][:-1])]
+        vm_dict[vm_name] = val
+    day_num = sys.stdin.readline().strip()
+    day_num = int(day_num)
+    # data_index = host_num+2+vm_num+1
+    data = []
+    for i in range(day_num):
+        requirement = sys.stdin.readline().strip()
+        requirement = int(requirement)
+        data_i = []
+        for i in range(requirement):
+            line = sys.stdin.readline().strip()
+            data_i.append(line)
+        data.append(data_i)
+host_dict_keys_list = list(host_dict.keys())
+HOST_DICT_LEN = len(host_dict_keys_list)
 
 class Host:
     def __init__(self, name):
@@ -58,7 +93,7 @@ class Host:
             elif self.B_cpu>=cpu and self.B_mem>=mem:
                 self.B_cpu -= cpu
                 self.B_mem -= mem
-                return  'B'
+                return 'B'
             else:
                 return 'NULL'
         else:
@@ -67,19 +102,35 @@ class Host:
                 self.B_cpu -= cpu/2
                 self.A_mem -= mem/2
                 self.B_mem -= mem/2
-                return  'ALL'
+                return 'ALL'
             else:
                 return 'NULL'
+
+    def available(self,vm):
+        info = vm_dict[vm]
+        cpu,mem,core = info
+        if core == 0:
+            if self.A_cpu >=cpu and self.A_mem>=mem: #优先放A节点
+                return True
+            elif self.B_cpu>=cpu and self.B_mem>=mem:
+                return True
+            else:
+                return False
+        else:
+            if self.A_cpu>=cpu/2 and self.A_mem>=mem/2 and self.B_cpu>=cpu/2 and self.B_mem>=mem/2:
+                return True
+            else:
+                return False
 
     def delvm(self,vm,type):
         info = vm_dict[vm]
         cpu, mem, core = info
         if type == 'A':
-            self.A_cpu -= cpu
-            self.A_mem -= mem
+            self.A_cpu += cpu
+            self.A_mem += mem
         elif type == 'B':
-            self.B_cpu -= cpu
-            self.B_mem -= mem
+            self.B_cpu += cpu
+            self.B_mem += mem
         else:
             self.A_cpu += cpu/2
             self.A_mem += mem/2
@@ -93,57 +144,58 @@ class hostList:
         self.id_info = {}
         self.out = []
 
-    def addHost(self):
-        list_for_choice = list(host_dict.keys())
-        lenth = len(list_for_choice)
-        choice = random.randint(0, lenth-1)
-        choice = list_for_choice[choice]
-        newHost = Host(choice)
-        self.allHost.append(newHost)
+    def addHost(self,vm_name):
+        random.shuffle(host_dict_keys_list)
+        for i in range(HOST_DICT_LEN):
+            host = Host(host_dict_keys_list[i])
+            if host.available(vm_name):
+                break
+        self.allHost.append(host)
 
     def search_and_add(self, vm_name,vm_id):
-        while True:
-
-            if len(self.allHost) == 0:
-                self.addHost()
-            length = len(self.allHost)
-            res = self.allHost[length-1].putvm(vm_name)
+        if len(self.allHost) == 0:
+            self.addHost(vm_name)
+        length = len(self.allHost)
+        for i in range(length):
+            res = self.allHost[i].putvm(vm_name)
             if res != 'NULL':
-                if res == 'A':
-                    self.out.append([length-1,'A'])
-                    # self.id_info[vm_id] = [vm_name, i, 'A']
-                    return
-                elif res == 'B':
-                    self.out.append([length-1, 'B'])
-                    # self.id_info[vm_id] = [vm_name, i, 'B']
-                    return
-                else:
-                    self.out.append([length-1])
-                    # self.id_info[vm_id] = [vm_name, i, 'ALL']
-                    return
-            else:
-                self.addHost()
+                self.upate_out(i,res,vm_id,vm_name)
+                return
+        self.addHost(vm_name)
+        res = self.allHost[-1].putvm(vm_name)
+        self.upate_out(len(self.allHost)-1,res,vm_id,vm_name)
 
+
+    def upate_out(self,i,AorB,vm_id,vm_name):
+        if AorB == 'A':
+            self.out.append([i,'A'])
+            self.id_info[vm_id] = [vm_name, i, 'A']
+        elif AorB == 'B':
+            self.out.append([i, 'B'])
+            self.id_info[vm_id] = [vm_name, i, 'B']
+        else:
+            self.out.append([i])
+            self.id_info[vm_id] = [vm_name, i, 'ALL']
 
     def del_vm(self, id):
-        vm_name, host_id, type = self.id_info[id]
-        self.allHost[host_id].delvm(vm_name,type)
+        vm_name,host_id,type = self.id_info[id]
+        self.allHost[host_id].delvm(vm_name, type)
 
 def main():
     my_hostList = hostList()
     out = []
     index = 0
     index_id = {}
-    for a,day in enumerate(data):
+    ITER = 0
+    for day in data:
         for line in day:
             add_or_del = line.split(',')[0][1:]
             ID = int(line.split(',')[-1][:-1])
             if add_or_del == 'add':
                 vm_name = line.split(',')[1].strip()
                 my_hostList.search_and_add(vm_name, ID)
-            # else:
-            #     # my_hostList.del_vm(ID)
-            #     print('test')
+            else:
+                my_hostList.del_vm(ID)
         start_id = index
         temp = index
         index = len(my_hostList.allHost)
@@ -176,13 +228,18 @@ def main():
 
         out += new_operate
         my_hostList.out.clear()
+        ITER +=1
+        if TIME:
+            if ITER % 100 == 0:
+                print('over')
         # if a%100 == 0:
         #     print('over')
 
     # with open('result.txt','w')as f:
     #     f.write('\n'.join(out))
-    # end = time.time()
-    # print('time consume',start-end)
+    end = time.time()
+    if TIME:
+        print('time consume',end-start)
     sys.stdout.write('\n'.join(out))  # 借
     sys.stdout.flush()
 if __name__ == "__main__":
